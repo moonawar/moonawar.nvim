0013df1e15fc6739b26a7bcb539196a34d1b18ca
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-01-17 04:16:27 +0700

    feat: keymap for fast git push

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,7 +2,9 @@ vim.keymap.set("n", "<leader>g", vim.cmd.Git)
 
 -- Fugitive push
 function Confirm_git_push()
-    local choice = vim.fn.input("Push? (y/n): ")
+    local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD")
+    branch = string.gsub(branch, "\n", "")
+    local choice = vim.fn.input("Push to " .. branch .. "? (y/n): ")
     print("\n")
     if string.lower(choice) == "y" then
         vim.cmd(":Git push")
