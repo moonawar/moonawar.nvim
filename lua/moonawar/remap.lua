21732810ff9199211715bcd0f5aaecb42d1be9e2
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-01-16 07:32:17 +0700

    feat: add some pog remap (thanks primeagen)

--- lua/moonawar/remap.lua
+++ lua/moonawar/remap.lua
@@ -3,3 +3,6 @@ vim.keymap.set('n', '<C-e>', vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "y", "\"+y")
+vim.keymap.set("n", "p", "\"+p")
