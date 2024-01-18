25e8776c76ef742d6e921ca3aeeeafca2d72c35e
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-01-18 08:34:19 +0700

    feat: harpoon remap

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,23 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+function Harpoon_add_file()
+    mark.add_file()
+    print(vim.fn.expand('%:p') .. " added to the harpoon")
+end
 
-vim.keymap.set("n", "<C-n>", ui.nav_next)
-vim.keymap.set("n", "<C-b>", ui.nav_prev)
+function Harpoon_next()
+    ui.nav_next()
+    print(vim.fn.expand('%:p'))
+end
+
+function Harpoon_prev()
+    ui.nav_prev()
+    print(vim.fn.expand('%:p'))
+end
+
+vim.keymap.set("n", "<leader>a", Harpoon_add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<c-n>", Harpoon_next)
+vim.keymap.set("n", "<c-b>", Harpoon_prev)
