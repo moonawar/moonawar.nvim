6bb9b770f7ec265ab666c1f1237208339ed86c1a
Addin Munawwar <moonawar@protonmail.com>
2024-01-18 09:44:24 +0700

    feat: map command capital to lower and lazy updates

---
+++ lua/moonawar/command.lua
@@ -0,0 +1,10 @@
+local commands = {'w', 'q'}
+
+for i=1, #commands do
+    vim.api.nvim_create_user_command(string.upper(commands[i]),
+        function ()
+            vim.cmd(commands[i])
+        end,
+        {}
+    )
+end
