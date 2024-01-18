dd09e4a1dc8c968286a99688a083204a751cf770
Addin Munawwar <moonawar@protonmail.com>
2024-01-18 10:09:16 +0700

    fix: missing require command.lua

--- lua/moonawar/init.lua
+++ lua/moonawar/init.lua
@@ -1,6 +1,7 @@
 require("moonawar.set")
 require("moonawar.remap")
 require("moonawar.lazy")
+require("moonawar.command")
 
 print("Welcome back Addin")
 require 'nvim-treesitter.install'.compilers = { "clang" }
